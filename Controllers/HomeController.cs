ccc242ec010b7c404d9dcdf36e16c823f759f1b9
Alex Zhideev <ZAlex1988@gmail.com>
2020-11-21 16:56:11 -0500

    MVC Views ViewControllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,22 @@ namespace Assignment4.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult About()
+        {
+
+            return View();
+        }
+
+        public IActionResult Reservations()
+        {
+
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+
+            return View();
+        }
     }
 }
